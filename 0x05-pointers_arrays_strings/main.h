b4ba098b9533c6728ee84fcf18b41843583daf13
Karenmiano <mianokaren@gmail.com>
2023-02-28 13:18:40 -0800

    Copies a string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,11 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 int _putchar(char c);
 void print_rev(char *s);
+char *_strcpy(char *dest, char *src);
+void print_array(int *a, int n);
+void puts_half(char *str);
+void puts2(char *str);
+void rev_string(char *s);
+void _puts(char *str);
 
 #endif
