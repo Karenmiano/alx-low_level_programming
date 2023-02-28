6be9dc61c35a887bbfdd404ab1d865519d55e563
Karenmiano <mianokaren@gmail.com>
2023-02-28 12:21:16 -0800

    Reversing strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses the characters provided by
+ * the pointer to a string
+ * @s:the input
+ */
+void print_rev(char *s)
+{
+	int i;
+	int m;
+
+	for (i = 0; *(s + i) != 0;)
+		i++;
+	m = i - 1;
+	while (m >= 0)
+	{
+		_putchar (*(s + m));
+		m--;
+	}
+}
