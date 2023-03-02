09e1761d5c17d5290d8dd2abb5c0240acdb20982
Karenmiano <mianokaren@gmail.com>
2023-03-02 00:59:03 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * _strncpy - the function copies the content in one string to
+ * another, taking a specified number of characters
+ * copying by replacing the first n characters
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of characters
+ * Return: dest
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
