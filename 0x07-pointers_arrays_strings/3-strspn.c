89af828234f214111c28e71bc868f4b3ce94f853
Karenmiano <mianokaren@gmail.com>
2023-03-06 07:11:31 -0800

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _strspn - function that Returns the number
+ * of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ * @s: string being checked
+ * @accept: reference characters
+ * Return: n
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+	unsigned int n;
+
+	i = 0;
+	n = 0;
+	k = 1;
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		while (accept[i] != '\0')
+		{
+			if (accept[i] == s[j])
+			{
+				n++;
+			}
+			i++;
+		}
+		i = 0;
+		if (n == 0)
+			return (n);
+		if (n == k)
+			k++;
+		else
+			return (n);
+	}
+	return (n);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
