252c62be852639b190e50ae20c3eee94be9a346b
Karenmiano <mianokaren@gmail.com>
2023-03-07 06:31:51 -0800

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ unsigned int _strspn(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
