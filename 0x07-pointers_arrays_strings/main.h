7799d7be698f551498cc9d141ca562fac3e83441
Karenmiano <mianokaren@gmail.com>
2023-03-07 18:14:28 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,5 +9,6 @@ void print_diagsums(int *a, int size);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 
 #endif
