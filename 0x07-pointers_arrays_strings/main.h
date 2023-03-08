a03ba5a06d926de62751d542f435178229b961e2
Karenmiano <mianokaren@gmail.com>
2023-03-07 18:18:12 -0800

    strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 void set_string(char **s, char *to);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+char *_strchr(char *s, char c);
 
 #endif
