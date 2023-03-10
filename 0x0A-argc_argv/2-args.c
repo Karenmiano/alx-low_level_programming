15d6397089496819501cf859ffc95843a93ca93f
Karenmiano <mianokaren@gmail.com>
2023-03-10 10:28:09 -0800

    Print the arguments given

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - accepts command line arguments and prints them out
+ * @argc: number of arguments
+ * @argv: array of pointers to the arguments
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	while (argc--)
