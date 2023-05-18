fbb8925bc9b5a68063daa4fcbcf37cf8311e3d52
Karen Miano <mianokaren1798@gmail.com>
2023-05-18 05:52:26 -0700

    Program that prints "Programming is like building a multilingual puzzle using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - starting point of our program
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
