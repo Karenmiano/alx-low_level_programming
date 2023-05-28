f03e2e5bfcd3fb104a3bd6f17d8111fdce695b07
Karen Miano <mianokaren1798@gmail.com>
2023-05-28 06:02:13 -0700

    Function that prints any integer using _putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints integers no matter the length
+ * @n: the integer
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int d = 10;
+	int divider = 1;
+	int num_digits = 1;
+	int temp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (temp / d)
+	{
+		num_digits++;
+		temp /= d;
+	}
+	while (num_digits > 1)
+	{
+		divider *= d;
+		num_digits--;
+	}
+	while (divider)
+	{
+		_putchar('0' + (n / divider));
+		n %= divider;
+		divider /= d;
+	}
+}
