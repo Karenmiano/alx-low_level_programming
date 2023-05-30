5a122c230d90ff93302d305f8a17d8ab8b74c946
Karen Miano <mianokaren1798@gmail.com>
2023-05-30 11:45:16 -0700

    Function that prints every second character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function that prints every second character from the first
+ * @str: string to be used
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	_putchar(str[i]);
+	while (str[i + 1] != '\0' && str[i + 2] != '\0')
+	{
+		_putchar(str[i + 2]);
+		i += 2;
+	}
+	_putchar('\n');
+}
