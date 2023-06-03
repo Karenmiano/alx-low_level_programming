8d4c37706417c08e08433cf5336ac9b33beca31f
Karen Miano <mianokaren1798@gmail.com>
2023-06-03 11:46:36 -0700

    An implementation of strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  * _strncpy - function that copies n bytes on to another string
  * @dest: the string to be copied to
  * @src: string to copy from
+ * @n: number of characters to be copied from the source string
  * Return: 'dest'
  */
 char *_strncpy(char *dest, char *src, int n)
