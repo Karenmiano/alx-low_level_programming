8c4abfe78a5cfefa5089a590577836082f77b14c
Karen Miano <mianokaren1798@gmail.com>
2023-06-06 12:13:27 -0700

    An implementation of strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strstr - function that checks for a matching substring within a string
+ * @haystack: string to be checked
+ * @needle: substring to find
+ * Return: pointer to where substring starts, NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *temp;
+
+	if (*haystack == '\0' || *needle == '\0')
+	{
+		return (NULLL);
+	}
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			temp = haystack;
+			for (i = 0; needle[i] != '\0'; i++)
+			{
+				if (*temp != needle[i])
+				{
+					break;
+				}
+				temp++;
+			}
+			if (needle[i] == '\0')
+			{
+				return (haystack);
+			}
+		}
+		haystack++;
+	}
+	return (NULL);
+}	
