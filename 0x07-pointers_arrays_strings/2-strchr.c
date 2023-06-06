1dad84bb595331db6314b75957c576945e5b4a5e
Karen Miano <mianokaren1798@gmail.com>
2023-06-06 05:56:15 -0700

    An implementation of strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * _strchr - locates a character in a string
  * @s: address of the string
  * @c: the character to be searched
- * Return: pointer to thee first occurence of the character in
+ * Return: pointer to the first occurence of the character in
  * the string
  */
 char *_strchr(char *s, char c)
