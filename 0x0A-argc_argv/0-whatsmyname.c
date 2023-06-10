531b66b1303ac8370ce45542ecb9f377fdc7b1c6
Karen Miano <mianokaren1798@gmail.com>
2023-06-10 07:18:05 -0700

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of program, prints the name of program
+ * @argc: number of CL arguments
+ * @argv: array of pointers to CL arguments
+ * Return: 0 if successful
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
