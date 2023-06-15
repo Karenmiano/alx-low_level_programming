13543787a096f60bd0d9e3c4d67fd90505d2b40a
Karen Miano <mianokaren1798@gmail.com>
2023-06-15 03:55:29 -0700

    Function that concatenates n bytes of a string onto another

--- 0x0C-more_malloc_free/1-main.c
+++ 0x0C-more_malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *concat;
 
-    concat = string_nconcat("Best ", "School !!!", 42);
+    concat = string_nconcat("Best ", NULL, 42);
     printf("%s\n", concat);
     free(concat);
     return (0);
