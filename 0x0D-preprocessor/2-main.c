8730941309b4312666561dbf5cb17b7a33208700
Karen Miano <mianokaren1798@gmail.com>
2023-06-19 04:47:11 -0700

    Program that prints the name of the file it will be compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point, prints the name of the file it will
+ * be compiled from
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
