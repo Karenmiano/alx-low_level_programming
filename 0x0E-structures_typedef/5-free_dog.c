5389b75b314b9867f38277b96b12eb01fee00e34
Karen Miano <mianokaren1798@gmail.com>
2023-06-20 03:33:04 -0700

    Function that frees a created struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees a struct dog(dog_t)
+ * @d: address of the struct
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
