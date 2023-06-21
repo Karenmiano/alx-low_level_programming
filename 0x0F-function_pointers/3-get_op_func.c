cf3fa395aee4c140e45f4b9e2129a8cfa4f9f2c4
Karen Miano <mianokaren1798@gmail.com>
2023-06-21 12:39:09 -0700

    Simple calculator

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 6)
+	{
+		if (*(ops[i].op) == *s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+	int (*action)(int, int);
+	char arr[] = {'+', '-', '*', '/', '%'};
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (strchr(arr, argv[2][0]) == NULL || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	action = get_op_func(&argv[2][0]);
+	printf("%d\n", action(a, b));
+	return (0);
+}
