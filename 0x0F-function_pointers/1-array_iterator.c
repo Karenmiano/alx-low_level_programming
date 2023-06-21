b4d7b63db7c07bfdddb59f57f03126280c2c311b
Karen Miano <mianokaren1798@gmail.com>
2023-06-21 05:20:43 -0700

    Function that executes given function on ech element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - executes the given function on each element of an array
+ * @array: start address of the array
+ * @size: size of the array
+ * @action: function to be executed
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
