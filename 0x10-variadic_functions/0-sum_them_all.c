23a8ff5d4269e1d1b165729db4e73a85afc1b517
Karen Miano <mianokaren1798@gmail.com>
2023-06-22 04:15:55 -0700

    Function that gets the sum of its arguments

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - gets the sum of integer arguments
+ * @n: the number of arguments
+ * Return: the result of the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list args;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
