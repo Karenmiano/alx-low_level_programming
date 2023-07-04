859553ed25151be84c933d48d6810781a4849fee
Karen Miano <mianokaren1798@gmail.com>
2023-07-04 06:02:04 -0700

    Function that frees a singly linked list and sets pointer to head node to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp1, *temp2;
 
+	if (head == NULL)
+		return;
 	temp1 = *head;
 	while (temp1 != NULL)
 	{
@@ -17,5 +19,4 @@ void free_listint2(listint_t **head)
 		free(temp2);
 	}
 	*head = NULL;
-	head = NULL;
 }
