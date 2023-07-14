01258db57312e39fff5f9545c737943d0c22796a
Karen Miano <mianokaren1798@gmail.com>
2023-07-14 14:09:42 -0700

    Function that creates a file and puts content into it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 	write(fd, text_content, strlen(text_content));
+	close(fd);
 	return (1);
 }
 /**
