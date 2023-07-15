83251da8bc17e8b81c72922c96fc862756b9e6e0
Karen Miano <mianokaren1798@gmail.com>
2023-07-14 18:32:26 -0700

    Function that copies content of a file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * main - starting point, copies content of a file to another
+ * @ac: number of arguments passed to main
+ * @argv: array of pointers to the arguments
+ * Return: 0 if successful
+ */
 int main(int ac, char *argv[])
 {
 	int fd1, fd2, chars_read = -1, chars_write;
@@ -8,48 +14,35 @@ int main(int ac, char *argv[])
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit (97);
+		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		fail_read_write(98, argv[1]);
 	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_APPEND | O_CREAT, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		fail_read_write(99, argv[2]);
 	while (chars_read != 0)
 	{
-		chars_read = read(fd1, buf, 1023);
+		chars_read = read(fd1, buf, 1024);
 		if (chars_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+			fail_read_write(98, argv[1]);
 		buf[chars_read] = '\0';
 		chars_write = write(fd2, buf, _strlen(buf));
 		if (chars_write == -1)
-		{
-			dprintf(STDERR_FILENO, "Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			fail_read_write(99, argv[2]);
 	}
 	if (close(fd1))
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd1);
-		exit(100);
-	}
+		fail_close(fd1);
 	if (close(fd2))
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
-		exit(100);
-	}
+		fail_close(fd2);
 	return (0);
 }
+/**
+ * _strlen - finds the length of a string
+ * @s: address of the string
+ * Return: lenth of the string
+ */
 int _strlen(char *s)
 {
 	int i = 0;
@@ -59,4 +52,31 @@ int _strlen(char *s)
 		i++;
 	}
 	return (i);
+}
+/**
+ * fail_read_write - sends error message to stderr if opening, reading or
+ * writing fails
+ * @i: integer for error message
+ * @file_name: name of file
+ * Return: nothing
+ */
+void fail_read_write(int i, char *file_name)
+{
+	if (i == 98)
+	{
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", file_name);
+		exit(98);
+	}
+	dprintf(STDERR_FILENO, "Can't write to %s\n", file_name);
+	exit(99);
+}
+/**
+ * fail_close - sends error message to stderr if file fails to close
+ * @i: file descriptor
+ * Return: nothing
+ */
+void fail_close(int i)
+{
+	dprintf(STDERR_FILENO, "Can't close fd %d\n", i);
+	exit(100);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void fail_read_write(int i, char *file_name);
+void fail_close(int i);
 
 #endif
