fcb56d86437844e441e4d8ffce8c2c51676beba0
Karen Miano <mianokaren1798@gmail.com>
2023-08-25 13:11:09 -0700

    Creating a c library with simple arithmetic operations functions then calling it from a python program

---
+++ 0x18-dynamic_libraries/simple_ops.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * op_add - performs addition
+ * @a: integer
+ * @b: integer
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - performs subtraction
+ * @a: integer
+ * @b: integer
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - performs multiplication
+ * @a: integer
+ * @b: integer
+ * Return: a x b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - performs division
+ * @a: integer
+ * @b: integer
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - performs modulus op.
+ * @a: integer
+ * @b: integer
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
