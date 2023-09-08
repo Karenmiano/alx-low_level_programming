58cf415b5f9e2ead668d907af348932150ac5e8c
Karen Miano <mianokaren1798@gmail.com>
2023-09-08 15:05:02 -0700

    Function that adds a new element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,8 +12,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node, *update;
 	unsigned long int index;
+	char *val;
 
 	if (ht == NULL || key[0] == '\0' || key == NULL || value == NULL)
+		return (0);
+	val = strdup(value);
+	if (val == NULL)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	update = ht->array[index];
@@ -22,16 +26,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		if (strcmp(update->key, key) == 0)
 		{
 			free(update->value);
-			update->value = strdup(value);
+			update->value = val;
 			return (1);
 		}
 		update = update->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
+		free(val);
 		return (0);
+	}
 	new_node->key = (char *)key;
-	new_node->value = strdup(value);
+	new_node->value = val;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
