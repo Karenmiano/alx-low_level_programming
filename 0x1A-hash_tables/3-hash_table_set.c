d7aa3d2ca095f2654b20ffc2494aca765011f4b6
Karen Miano <mianokaren1798@gmail.com>
2023-09-08 12:20:32 -0700

    Function that inserts a new element in a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	update = ht->array[index];
-	while(update != NULL)
+	while (update != NULL)
 	{
 		if (strcmp(update->key, key) == 0)
 		{
