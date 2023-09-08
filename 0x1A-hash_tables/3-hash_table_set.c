df0b99be27dcaaa217eec31a6663ba5d94c155be
Karen Miano <mianokaren1798@gmail.com>
2023-09-08 09:09:31 -0700

    Function that adds a new key/value pair to a hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (ht->array == NULL)
 		return (0);
-	if (key[0] == '\0')
+	if (key == NULL || key[0] == '\0')
 		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
